0c25d4f7aae25a3111b8971a7d029b845088ce5a
Brian L. Troutwine <brian@troutwine.us>
2013-09-11 10:16:35 -0700

    Some misc. v1to2.sh changes.
    Signed-off-by: Brian L. Troutwine <brian@troutwine.us>

--- v1to2.sh
+++ v1to2.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+STARTDIR=`pwd`
+
+/tmp/beat/bin/beat-0.1.0 stop || echo "No previous node running."
 rm -rf /tmp/beat
 rm -rf _rel
 
@@ -17,13 +20,20 @@ cp _rel/beat-0.1.1.tar.gz /tmp/beat-0.1.1.tar.gz
 
 cd /tmp/beat
 tar xf /tmp/beat-0.1.0.tar.gz
-./bin/beat-0.1.0 stop
+
+echo "STARTING NODE"
 ./bin/beat-0.1.0 start
 
 mkdir -p releases/0.1.1
 cp /tmp/beat-0.1.1.tar.gz releases/0.1.1/beat.tar.gz
 
+sleep 5s # let the system boot
+
+echo "UPGRADING NODE TO 0.1.1"
 ./bin/beat-0.1.0 upgrade "0.1.1/beat"
 
 rm /tmp/beat-0.1.0.tar.gz
 rm /tmp/beat-0.1.1.tar.gz
+
+cd $STARTDIR
+git checkout master
